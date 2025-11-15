b26b84422b3c11d6fb8c9593d03b70cb7f468b69
Nitsan Bracha <nitsi.br@gmail.com>
2025-11-15 21:23:21 +0200

    [feature] Add presence and activity timestamps to session management
    Enhanced the SessionManagerCore class by including user presence and last activity timestamps in session data. This update improves session tracking and provides better insights into user activity, contributing to a more robust session management system.

--- src/core/manager.core.ts
+++ src/core/manager.core.ts
@@ -383,6 +383,10 @@ export class SessionManagerCore extends SessionManager implements OnModuleInit {
         status: session.status,
         config: session.sessionConfig,
         me: me,
+        presence: session.presence,
+        timestamps: {
+          activity: session?.getLastActivityTimestamp(),
+        },
       });
     }
 
@@ -411,6 +415,10 @@ export class SessionManagerCore extends SessionManager implements OnModuleInit {
           status: WAHASessionStatus.STOPPED,
           config: config || {},
           me: null,
+          presence: null,
+          timestamps: {
+            activity: null,
+          },
         });
       }
     }
